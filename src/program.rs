6cf364d6d08255470d67d19e81f6f00f92f31208
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-06-10 10:28:52 +0200

    feat: Sort Program

--- src/program.rs
+++ src/program.rs
@@ -1,5 +1,27 @@
+use std::cmp::Ordering;
+
 pub struct Program {
     instr: Vec<(String, String)>,
 }
 
-impl Program {}
+fn bin_to_u32(s: &str) -> u32 {
+    u32::from_str_radix(s, 2).unwrap()
+}
+
+impl Program {
+    pub fn sort(&mut self) {
+        self.instr.sort_by(|a, b| {
+            let a = bin_to_u32(&a.0);
+            let b = bin_to_u32(&b.0);
+            match a.cmp(&b) {
+                Ordering::Less => Ordering::Greater,
+                Ordering::Equal => Ordering::Equal,
+                Ordering::Greater => Ordering::Less,
+            }
+        });
+    }
+
+    pub fn eval(to_decipher: String) -> String {
+        todo!()
+    }
+}
